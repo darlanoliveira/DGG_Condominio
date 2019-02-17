96bf3a3d31078c7b94f7d367196c9325a899df06
darlanoliveira <darlan.first@gmail.com>
2019-02-17 10:50:17 -0300

    Push Inicial

---
+++ DGG_Condominio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DGG_Condominio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
